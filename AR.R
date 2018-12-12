e2fa58f8a24ba191f9ca9c1f7632dafe58756df3
arthurseveral <arthurseveral@gmail.com>
2018-12-12 14:13:45 -0200

    Incluído chamada para função arima

--- AR.R
+++ AR.R
@@ -19,6 +19,8 @@ for(i in 2:100) {
 
 AR.LSE(series, 1);
 
+arima(series, order=c(1, 0, 0));
+
 series <- w <- rnorm(100);
 
 for(i in 3:100) {
@@ -27,8 +29,14 @@ for(i in 3:100) {
 
 AR.LSE(series, 2);
 
+arima(series, order=c(2, 0, 0));
+
+series <- w <- rnorm(100);
+
 for(i in 4:100) {
     series[i] <- w[i] + 0.6 * w[i - 1] + 0.45 * w[i - 2] + 0.35 * w[i - 3];
 }
 
 AR.LSE(series, 3);
+
+arima(series, order=c(3, 0, 0));
